58c184ad4207a51e75a02cb2ee85dc4d9163217a
Starwhooper <thiemo@schuff.eu>
2021-06-21 21:03:50 +0200

    Add files via upload

--- RandomImagewithinCommons.php
+++ RandomImagewithinCommons.php
@@ -6,10 +6,10 @@ $wgExtensionCredits['parserhook'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'Random Image within Commons',
 	'descriptionmsg' => 'riwc-desc',
-	'version'        => '2.2 beta build 20140830',
+	'version'        => '2.3 beta build 20140916',
 	'author'         => '[http://www.mediawiki.org/wiki/User:Starwhooper Thiemo Schuff]',
 	'url'            => 'http://www.mediawiki.org/wiki/Extension:RandomImagewithinCommons',
-	'license-name'  => 'cc-by-sa-de 4.0',
+	'license-name'  => 'cc-by-sa 4.0',
 );
 
 $wgExtensionMessagesFiles['riwc'] = dirname(__FILE__) . '/RandomImagewithinCommons.i18n.php';
@@ -22,48 +22,29 @@ function wfRandomImageFunction() {
  
 function wfRandomImagewithinsCommons($input, array $args, Parser $parser, PPFrame $frame) {
 	global $wgriwc;
-	
+
 	$parser->disableCache();
-	$output = NULL;
-	$imagefound = false;
-	$dbr = wfGetDB( DB_SLAVE );
 	
-	while ($output == NULL){
+	//set defaults
+	if (!isset($wgriwc['size'])) $wgriwc['size'] = '200px';
+	
+	//connect DB
+	$dbr = wfGetDB( DB_SLAVE );	
+	
+	//Get forbitten files from blacklist
+	$res = $dbr->select(array('categorylinks','page'),array('page_title'),array('cl_to = "ricw_blacklist"','cl_type = "file"'),__METHOD__,array(),array('page' => array( 'INNER JOIN',array('cl_from = page_id'))));
+	foreach($res as $row) $conditions[] = 'il_to != "'.$row->page_title.'"';
+	
+	//Get Imagename
+	$imagename = $dbr->selectField('imagelinks', 'il_to', $conditions, __METHOD__, array('GROUP BY' => 'il_to', 'ORDER BY' => 'RAND()'));
 
-		//Get IDs of forbitten files
-		$res = $dbr->select('categorylinks', array('cl_from'), 'cl_to = "ricw_blacklist" and cl_type = "file"', $fname = 'Database::select',array());
-		foreach($res as $row) $forbitten_cl_from = 'page_id = '.$row->cl_from.' or';
-		$forbitten_cl_from = substr($blacklist,0,-3);
-
-		//Get Filename of forbitten files
-		$res = $dbr->select('page', array('page_title'), $forbitten_cl_from, $fname = 'Database::select',array());
-		foreach($res as $row) $conditions[] = 'il_to != "'.$row->page_title.'"';
+	//Get Articles with the image
+	$res = $dbr->select(array('imagelinks','page'),array('page_title'),array('il_to = "'.$imagename.'"','page_namespace = 0'),__METHOD__,array(),array('page' => array( 'INNER JOIN',array('il_from = page_id'))));
+	foreach($res as $row) $article['title'] .= '[['.str_replace('_',' ',$row->page_title).']] & ';
+	$article['title'] = substr($article['title'],0,-3);		
 		
-		//Get Imagename
-		$res = $dbr->select('imagelinks', array( 'il_to' ), $conditions, $fname = 'Database::select', array('LIMIT' => 1, 'GROUP BY' => 'il_to', 'ORDER BY' => 'RAND()'));
-		foreach($res as $row) {
-			$imagename = $row->il_to;
-			break;
-		}
+	//Prompt Tag
+	$output = $parser->recursiveTagParse( '<div>[[File:'.$imagename.'|'.$wgriwc['size'].']]<br /> '.wfMessage('riwc-fromarticle').': '.$article['title'].'</div>');
 		
-		//check if imagefilesize good enought
-		if(wfFindFile($imagename)->mInfo['size'] <= 1000) continue;;
-		
-		//Get Article IDs with the image
-		$res = $dbr->select('imagelinks', array('il_from'), 'il_to = "'.$imagename.'"', $fname = 'Database::select', array());
-		$articlelistconditions = '(';
-		foreach($res as $row) $articlelistconditions .= 'page_id = '.$row->il_from.' or ';
-		$articlelistconditions = substr($articlelistconditions,0,-4) . ') and page_namespace = 0';
-		
-		//Get Article with the Image
-		$res = $dbr->select('page', array( 'page_title' ), $articlelistconditions, $fname = 'Database::select', array());
-		foreach($res as $row) $article['title'] .= '[['.str_replace('_',' ',$row->page_title).']] & ';		
-		$article['title'] = substr($article['title'],0,-3);		
-
-		//Prompt Tag
-		if (!isset($wgriwc['size'])) $wgriwc['size'] = '200px';
-		$output = $parser->recursiveTagParse( '<div>[[File:'.$imagename.'|'.$wgriwc['size'].']]<br /> '.wfMessage('riwc-fromarticle').': '.$article['title'].'</div>');
-		
-	}
 	return 	$output;
 }
